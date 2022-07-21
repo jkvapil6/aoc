6aa03e481c4bbe00cd19735cda677bc5eb36ec10
Jan Kvapil <jkvapil6@gmail.com>
2022-07-21 20:04:52 +0200

    aoc15 f# init

---
+++ 15/Program.fs
@@ -0,0 +1,17 @@
+﻿open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let mutable exitCode = 0
+    let str: string option  = 
+        try 
+            Some (File.ReadAllText("input.txt"))
+        with 
+            | :? IOException -> printfn "File can not be readed!"; None;
+
+    match str with 
+        | Some x -> printfn "%s" x
+        | None -> exitCode <- -1
+    
+    exitCode
+    
